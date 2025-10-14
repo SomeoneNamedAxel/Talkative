1214b1d1378cfe59b8732e7485cd11c55f5a6f07
SomeoneNamedAxel <bidault.axel.antoine@gmail.com>
2025-10-14 21:36:59 +0200

    Token is now a struct with designation and value fields

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,89 +1,135 @@
+pub(crate) struct TokensLines {
+    lines: Vec<TokensLine>,
+}
 
-    pub(crate) struct TokensLines {
-        lines: Vec<TokensLine>,
+impl TokensLines {
+    pub(crate) fn from_string(string: String, delimiters: Vec<char>, keywords: Vec<String>, operators: Vec<String>) -> Self {
+        TokensLines {
+            lines: string
+                .lines()
+                .map(|line| TokensLine::new(line.to_string(), &delimiters, &keywords, &operators))
+                .collect(),
+        }
     }
 
-    impl TokensLines {
-        pub(crate) fn from_string(string: String, delimiters: Vec<char>) -> Self {
-            TokensLines {
-                lines: string.lines().map(|line|
-                    TokensLine::new(line.to_string(), delimiters.clone())
-                ).collect(),
+    pub(crate) fn to_vec(&self) -> Vec<Vec<Token>> {
+        self.lines.iter().map(|line| line.to_vec()).collect()
+    }
+
+    pub(crate) fn to_string_vec(&self) -> Vec<Vec<String>> {
+        self.lines.iter().map(|line| line.to_string_vec()).collect()
+    }
+
+    pub(crate) fn merge_within_delimiters(&mut self, delimiter: char, dissolve_delimiters: bool) {
+        self.lines
+            .iter_mut()
+            .map(|line| line.merge_within_delimiters(delimiter))
+            .collect()
+    }
+}
+
+struct TokensLine {
+    line: Vec<Token>,
+}
+
+impl TokensLine {
+    fn new(string: String, delimiters: &Vec<char>, keywords: &Vec<String>, operators: &Vec<String>) -> Self {
+        let mut tokens: Vec<String> = Vec::new();
+        let mut current_token = String::new();
+
+        for character in string.chars() {
+            if delimiters.contains(&character) {
+                tokens.push(current_token.clone());
+                current_token.clear();
+
+                tokens.push(character.to_string());
+            } else {
+                current_token.push(character);
             }
         }
+        tokens.push(current_token.clone());
 
-        pub(crate) fn to_vec(&self) -> Vec<Vec<String>> {
-            self.lines.iter().map(|line| line.to_vec()).collect()
-        }
-
-        pub(crate) fn merge_within_delimiters(&mut self, delimiter: char, dissolve_delimiters: bool) {
-            self.lines.iter_mut().map(|line| line.merge_within_delimiters(delimiter, dissolve_delimiters)).collect()
+        TokensLine {
+            line: tokens
+                .into_iter()
+                .filter(|token_str| !token_str.is_empty())
+                .map(|token_str| Token::new(Self::identify_token(&token_str, &keywords, &operators), token_str))
+                .collect(),
         }
     }
 
-    struct TokensLine {
-        line: Vec<String>,
+    fn identify_token(token: &String, keywords: &Vec<String>, operators: &Vec<String>) -> String {
+        if keywords.contains(&token) {
+            String::from("keyword")
+        } else if operators.contains(&token) {
+            String::from("operator")
+        } else if *token == " ".to_string() || *token == "\"" {
+            String::from("separator")
+        } else {
+            String::from("literal")
+        }
     }
 
-    impl TokensLine {
-        fn new(string: String, delimiters: Vec<char>) -> Self {
-            let mut tokens: Vec<String> = Vec::new();
-            let mut current_token = String::new();
+    fn to_vec(&self) -> Vec<Token> {
+        self.line.clone()
+    }
 
-            for character in string.chars() {
-                if delimiters.contains(&character) {
-                    tokens.push(current_token.clone());
-                    current_token.clear();
+    fn to_string_vec(&self) -> Vec<String> {
+        self.line.iter().map(|line| line.to_string()).collect()
+    }
 
-                    tokens.push(character.to_string());
+    fn merge_within_delimiters(&mut self, delimiter: char) {
+        let mut new_line: Vec<Token> = Vec::new();
+
+        let mut token_built = Token::new("literal".parse().unwrap(), String::new());
+        let mut is_merging_active = false;
+        let mut is_merging_state_just_changed;
+
+        for token in self.line.clone() {
+            is_merging_state_just_changed = false;
+
+            if token
+                .value
+                .parse::<char>()
+                .is_ok_and(|token| token == delimiter)
+            {
+                is_merging_active = !is_merging_active;
+
+                if !is_merging_active {
+                    new_line.push(token_built.clone());
+                }
+
+                is_merging_state_just_changed = true;
+            }
+
+            if is_merging_active {
+                if is_merging_state_just_changed {
+                    new_line.push(token);
                 } else {
-                    current_token.push(character);
+                    token_built =
+                        Token::new(token_built.designation, token_built.value + &*token.value);
                 }
-            }
-            tokens.push(current_token.clone());
-
-            TokensLine {
-                line: tokens.iter().filter(|token| !token.is_empty()).cloned().collect()
+            } else {
+                new_line.push(token);
             }
         }
 
-        fn to_vec(&self) -> Vec<String> {
-            self.line.clone()
-        }
-
-        fn merge_within_delimiters(&mut self, delimiter: char, dissolve_delimiters: bool) {
-            let mut new_line: Vec<String> = Vec::new();
-
-            let mut token_built = String::new();
-            let mut is_merging_active = false;
-            let mut is_merging_state_just_changed ;
-
-            for token in self.line.clone() {
-
-                is_merging_state_just_changed = false;
-
-                if token.parse::<char>().is_ok_and(|token| token == delimiter) {
-                    is_merging_active = !is_merging_active;
-
-                    if !is_merging_active {
-                        new_line.push(token_built.clone());
-                    }
-
-                    is_merging_state_just_changed = true;
-                }
-
-                if is_merging_active {
-                    if is_merging_state_just_changed {
-                        new_line.push(token);
-                    } else {
-                        token_built.push_str(token.as_str());
-                    }
-
-                } else {
-                    new_line.push(token.to_string());
-                }
-            }
-
-            self.line = new_line;
-        }
+        self.line = new_line;
     }
+}
+
+#[derive(Clone, Debug)]
+pub(crate) struct Token {
+    designation: String,
+    value: String,
+}
+
+impl Token {
+    fn new(designation: String, value: String) -> Self {
+        Token { designation, value }
+    }
+
+    fn to_string(&self) -> String {
+        format!("({}) {}", self.designation, self.value)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@ fn main() {
 
     let delimiters = ['"', ' '].to_vec();
 
-    let known_keywords = ["chaine".to_string(), "afficher".to_string()].to_vec();
-    let known_operators = [":".to_string(), "+".to_string(), "-".to_string()].to_vec();
+    let keywords = ["chaine".to_string(), "afficher".to_string()].to_vec();
+    let operators = [":".to_string(), "+".to_string(), "-".to_string()].to_vec();
 
-    let mut result = lexer::TokensLines::from_string(contents, delimiters);
-
-    println!("{:?}", result.to_vec());
+    let mut result = lexer::TokensLines::from_string(contents, delimiters, keywords, operators);
 
     result.merge_within_delimiters('"', false);
 
-    println!("{:?}", result.to_vec());
+    println!("{:?}", result.to_string_vec());
 }
